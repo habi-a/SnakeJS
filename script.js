7b660d071664486979f078dbe1132cb85e29d90c
Açal Habi <habi_a@etna-alternance.net>
2019-09-28 20:16:14 +0200

    reformat code

--- script.js
+++ script.js
@@ -1,233 +1,234 @@
+class Snake {
+  constructor(speed) {
+    this.x = [0];
+    this.y = [0];
+    this.dx = [speed];
+    this.dy = [0];
+  }
+ 
+  changeDir(nextDir, speed) {
+    if (this.dx[0] && nextDir === 'up') {
+      this.dx[0] = 0;
+      this.dy[0] = -speed;
+    }
+    if (this.dx[0] && nextDir === 'down') {
+      this.dx[0] = 0;
+      this.dy[0] = speed;
+    }
+    if (this.dy[0] && nextDir === 'right') {
+      this.dx[0] = speed;
+      this.dy[0] = 0;
+    }
+    if (this.dy[0] && nextDir === 'left') {
+      this.dx[0] = -speed;
+      this.dy[0] = 0;
+    }
+  }
+
+  draw (context) {
+    for (var i = 0; i < this.x.length; i++) {
+      context.beginPath();
+      context.rect(this.x[i], this.y[i], 10, 10);
+      context.fillStyle = '#2B823A';
+      context.strokeStyle = '#012E34';
+      context.fill();
+      context.stroke();
+      context.closePath();
+    }
+  }
+
+  update() {
+    this.dx = Snake.unshiftAndCut(this.dx);
+    this.dy = Snake.unshiftAndCut(this.dy);
+  }
+
+  grow(iteration) {
+    for (var i = 0; i < iteration; i++) {
+      var lastX = this.x[this.x.length - 1];
+      var lastY = this.y[this.y.length - 1];
+      var lastDX = this.dx[this.dx.length - 1]
+      var lastDY = this.dy[this.dy.length - 1]
+      this.x.push(lastX - (Math.sign(lastDX) * 10));
+      this.y.push(lastY - (Math.sign(lastDY) * 10));
+      this.dx.push(lastDX);
+      this.dy.push(lastDY);
+    }
+  }
+
+  isEating(foodX, foodY) {
+    return this.x[0] === foodX && this.y[0] === foodY;
+  }
+
+  move() {
+    for (var i = 0; i < this.x.length; i++) {
+      this.x[i] += this.dx[i];
+      this.y[i] += this.dy[i];
+    }
+  }
+
+  collides(width, height) {
+    var x = this.x[0], y = this.y[0];
+    // Check collision with the wall
+    if (x < 0 || x + 10 > width || y < 0 || y + 10 > height)
+      return true;
+    // Check collision with itself
+    for (var i = 1; i < this.x.length; i++) {
+      if (x === this.x[i] && y === this.y[i])
+        return true;
+    }
+    return false;
+  }
+
+  reset(speed) {
+    this.x = [0];
+    this.y = [0];
+    this.dx = [speed];
+    this.dy = [0];
+  }
+
+  static unshiftAndCut(arr) {
+    var result = arr.slice();
+    return result.map(function(el, i) {
+      if (!i)
+        return arr[0];
+      return arr[i - 1];
+    });
+  }
+}
+
+class Food {
+  constructor(width, height, color) {
+    this.x = 0;
+    this.y = 0;
+    this.color = color;
+  }
+  
+  spawn(posX, posY) {
+      var matrix = [];
+      var free = [];
+      for (var i = 0; i < 20; i++) {
+        matrix[i] = Array(20).fill(0);
+      }
+      for (var i = 0; i < posX.length; i++) {
+        var x = Math.floor(posX[i] / 10);
+        var y = Math.floor(posY[i] / 10);
+        matrix[y][x] = 1;
+      }
+      for (var i = 0; i < 20; i++) {
+        for (var j = 0; j < 20; j++) {
+          if (!matrix[i][j])
+            free.push([i, j]);
+        }
+      }
+      var foodPos = Math.floor(Math.random() * free.length);
+      this.x = free[foodPos][1] * 10;
+      this.y = free[foodPos][0] * 10;
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.arc(this.x + 5, this.y + 5, 5, 0, Math.PI * 2);
+    context.fillStyle = this.color;
+    context.fill();
+    context.closePath();
+  }
+
+  reset() {
+    this.x = 0;
+    this.y = 0;
+  }
+}
+
 (function() {
-  var canvas      = document.getElementById("myCanvas");
+  var canvas      = document.getElementById('myCanvas');
+  var context     = canvas.getContext('2d');
   var msg         = document.getElementById('msg');
   var actualScore = document.getElementById('actual-score');
-  var ctx         = canvas.getContext("2d");
-  var gameOn      = 0;
-  var level       = 0
-  var speed       = 2;
-  var endGame     = new Event('endGame');
+  var levelScore  = document.getElementById('level');
+  var anim        = 0;
+  var new_game    = true;
+  var game_on     = 0;
   var score       = 0;
-  var anim;
+  var level       = 1;
+  var init_speed  = 2;
+  var speed       = 2;
+  var nextDir     = '';
+  var endGame     = new Event('endGame');
+  var snake       = new Snake(init_speed);
+  var food        = new Food(canvas.width, canvas.height, "#EDE916");
 
-  document.addEventListener('endGame', function endGameHandler(e) {
-    msg.innerHTML = 'Game Over! Press <em>Space</em> to start a new game'
-    gameOn = 0;
-    window.cancelAnimationFrame(anim);
-    score = 0;
-  }, false);
+  document.addEventListener('endGame', resetGame, false);
+  document.addEventListener('keydown', game, false);
 
-  document.addEventListener("keydown", function spaceHandler(e) {
-    if (e.keyCode === 32) {
-      e.preventDefault();
-      if (!gameOn) {
-        msg.innerHTML = 'Go!'
-        gameOn = 1;
-        game();
-      }
-    }
-  }, false);
 
-  function game() {
-    var snake = createSnake(speed);
-    var food = createFood(canvas.width, canvas.height, "#EDE916");
-    var candy = createCandy(canvas.width, canvas.height)
-    var nextDir = '';
-    var fps = 0;
-
-    document.addEventListener('keydown', getInputDirection, false);
-
-    function getInputDirection(e) {
+  function getInputDirection(e) {
+    if (!new_game) {
       switch (e.keyCode) {
         case 40:
-          e.preventDefault();
           nextDir = 'down';
           break;
         case 39:
-          e.preventDefault();
           nextDir = 'right';
           break;
         case 38:
-          e.preventDefault();
           nextDir = 'up';
           break;
         case 37:
-          e.preventDefault();
           nextDir = 'left';
           break;
         default:
       }
     }
+  }
 
-    function play() {
-      fps++;
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      if ((snake.x[0] % 10 === 0) && (snake.y[0] % 10 === 0)) {
-        snake.update();
-        snake.changeDir(nextDir);
-      }
-      snake.move();
-      if (snake.isEating(food.x, food.y)) {
+  function game(e) {
+    if (e.keyCode === 32) {
+      if (!game_on) {
+        msg.innerHTML = 'Go!';
+        game_on = 1;
+        document.addEventListener('keydown', getInputDirection, false);
         food.spawn(snake.x, snake.y);
-        snake.grow();
-        if (snake.x.length % 7 === 0) {
-          candy.active = true;
-          candy.spawn(snake.x, snake.y);
-          setTimeout(function() {
-            candy.active = false;
-          }, 3000);
-        }
-        score += 1;
-      }
-
-      if (candy.active && snake.isEating(candy.x, candy.y)) {
-        candy.active = false;
-        snake.grow();
-        snake.grow();
-        snake.grow();
-        score += 3;
-      }
-      if (candy.active) candy.draw(ctx);
-      food.draw(ctx);
-      snake.draw(ctx);
-
-      if (snake.collides(canvas.width, canvas.height)) {
-        document.dispatchEvent(endGame);
-      } else {
-        anim = window.requestAnimationFrame(play);
-      }
-      actualScore.innerText = '' + score;
-    }
-
-    food.spawn(snake.x, snake.y);
-    anim = window.requestAnimationFrame(play);
-  }
-
-  function createCandy(width, height) {
-    var _candy = createFood(width, height, "#ED0916");
-    _candy.active = false;
-    return _candy;
-  }
-
-  function createFood(width, height, color) {
-    return {
-      x: 0,
-      y: 0,
-      // spawn the food in a random free space
-      spawn: function(posX, posY) {
-        var matrix = [],
-          free = [],
-          x, y, foodPos, i, len;
-        for (i = 0; i < 20; i++) {
-          matrix[i] = Array(20).fill(0);
-        }
-        for (i = 0, len = posX.length; i < len; i++) {
-          x = Math.floor(posX[i] / 10);
-          y = Math.floor(posY[i] / 10);
-          matrix[y][x] = 1;
-        }
-
-        for (i = 0; i < 20; i++) {
-          for (var j = 0; j < 20; j++) {
-            if (!matrix[i][j])
-              free.push([i, j]);
-          }
-        }
-
-        foodPos = Math.floor(Math.random() * free.length);
-        this.x = free[foodPos][1] * 10;
-        this.y = free[foodPos][0] * 10;
-      },
-      draw: function(ctx) {
-        ctx.beginPath();
-        ctx.arc(this.x + 5, this.y + 5, 5, 0, Math.PI * 2);
-        ctx.fillStyle = color;
-        ctx.fill();
-        ctx.closePath();
+        anim = window.requestAnimationFrame(play);      
       }
     }
   }
 
-  function createSnake(speed) {
-    function unshiftAndCut(arr) {
-      var result = arr.slice();
-      return result.map(function(el, i) {
-        if (!i)
-          return arr[0];
-        return arr[i - 1];
-      });
+  function play() {
+    new_game = false;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    if ((snake.x[0] % 10 === 0) && (snake.y[0] % 10 === 0)) {
+      snake.update();
+      snake.changeDir(nextDir, speed);
+    }
+    snake.move();
+    if (snake.isEating(food.x, food.y)) {
+      food.spawn(snake.x, snake.y);
+      snake.grow(1);
+      score += 1;
     }
 
-    return {
-      x: [0],
-      y: [0],
-      dx: [speed],
-      dy: [0],
-      changeDir: function(nextDir) {
-        if (this.dx[0] && nextDir === 'up') {
-          this.dx[0] = 0;
-          this.dy[0] = -speed;
-        }
-        if (this.dx[0] && nextDir === 'down') {
-          this.dx[0] = 0;
-          this.dy[0] = speed;
-        }
-        if (this.dy[0] && nextDir === 'right') {
-          this.dx[0] = speed;
-          this.dy[0] = 0;
-        }
-        if (this.dy[0] && nextDir === 'left') {
-          this.dx[0] = -speed;
-          this.dy[0] = 0;
-        }
-      },
-      draw: function(ctx) {
-        for (var i = 0, len = this.x.length; i < len; i++) {
-          ctx.beginPath();
-          ctx.rect(this.x[i], this.y[i], 10, 10);
-          ctx.fillStyle = '#2B823A';
-          ctx.strokeStyle = '#012E34';
-          ctx.fill();
-          ctx.stroke();
-          ctx.closePath();
-        }
-      },
-      update: function() {
-        this.dx = unshiftAndCut(this.dx);
-        this.dy = unshiftAndCut(this.dy);
-      },
-      grow: function() {
-        var lastX = this.x[this.x.length - 1];
-        var lastY = this.y[this.y.length - 1];
-        var lastDX = this.dx[this.dx.length - 1]
-        var lastDY = this.dy[this.dy.length - 1]
-        this.x.push(lastX - (Math.sign(lastDX) * 10));
-        this.y.push(lastY - (Math.sign(lastDY) * 10));
-        this.dx.push(lastDX);
-        this.dy.push(lastDY);
-      },
-      isEating: function(foodX, foodY) {
-        return this.x[0] === foodX && this.y[0] === foodY;
-      },
-      move: function() {
-        for (var i = 0, len = this.x.length; i < len; i++) {
-          this.x[i] += this.dx[i];
-          this.y[i] += this.dy[i];
-        }
-      },
-      collides: function(width, height) {
-        var x = this.x[0],
-          y = this.y[0];
-        // Check collision with the wall
-        if (x < 0 || x + 10 > width || y < 0 || y + 10 > height)
-          return true;
-        // Check collision with itself
-        for (var i = 1, len = this.x.length; i < len; i++) {
-          if (x === this.x[i] && y === this.y[i])
-            return true;
-        }
-        return false;
-      }
+    food.draw(context);
+    snake.draw(context);
+
+    if (snake.collides(canvas.width, canvas.height)) {
+      document.dispatchEvent(endGame);
+    } else {
+      anim = window.requestAnimationFrame(play);
     }
+    levelScore.innerText  = '' + level;
+    actualScore.innerText = '' + score;
+  }
+
+  function resetGame() {
+    window.cancelAnimationFrame(anim);
+    game_on        = 0;
+    score         = 0;
+    level         = 1;
+    new_game      = true;
+    speed         = init_speed
+    nextDir       = ''
+    msg.innerHTML = 'Game Over! Press <em>Space</em> to start a new game';
+    snake.reset(init_speed);
+    food.reset();
   }
 })()
