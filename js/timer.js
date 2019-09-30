fa245abbbe6d8487aa3b1f83954cfb9da1b44e0e
Fr4nckyDumpi <courta_f@etna-alternance.net>
2019-09-30 10:19:34 +0200

    add icon (snake + eat) + add timer

---
+++ js/timer.js
@@ -0,0 +1,35 @@
+function startTimer() {
+    var timer = document.getElementById("timer").innerHTML;
+    var arr = timer.split(":");
+    var min = arr[0];
+    var sec = arr[1];
+
+    if (sec == 0) {
+        if (min == 0) {
+            alert("Time out");
+            window.location.reload();
+            return
+        }
+        min++;
+        if (min < 10) {
+            min = "0" + min;
+        }
+        sec = 59;
+    }
+    else {
+        sec--;
+    }
+
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+
+    document.getElementById("timer").innerHTML = min + ":" + sec;
+    setTimeout(startTimer,1000);
+}
+
+window.onload = function () {
+    var trentesec = 6 * 5,
+        display = document.querySelector('#timer');
+    startTimer(trentesec, display);
+};
